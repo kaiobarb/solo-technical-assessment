f27bc5a2d383e54e188f02cf49a57a506d87924c
Kaio <kaiobarb@gmail.com>
2022-12-24 00:10:16 -0800

    Changed data structure to search for recipes; mostly working!

--- coctail_search_engine.rb
+++ coctail_search_engine.rb
@@ -1,55 +1,65 @@
 require 'net/http'
 require 'json'
+require 'set'
 
-def get_recipes(type)
-    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{type}"
-    url = URI(url)
-    response = Net::HTTP.get(url)
-    data = JSON.parse(response)
-
-    instructions = []
-    data['drinks'].each do |drink|
-      instructions << drink['strInstructions']
+def get_recipes(type, instructions_lookup, ingredient_lookup)
+  url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{type}"
+  url = URI(url)
+  response = Net::HTTP.get(url)
+  data = JSON.parse(response)
+  
+  data['drinks'].each do |drink|
+    drink_id = drink['idDrink'].to_i()
+    instructions_lookup[drink_id] = drink['strInstructions']
+    15.times do |index|
+      ingredient = drink["strIngredient#{index+1}"]
+      if ingredient.nil?
+        break
+      end
+      if ingredient_lookup[ingredient].nil?
+        ingredient_lookup[ingredient] = [drink_id]
+      else 
+        ingredient_lookup[ingredient] << drink_id
+      end
     end
-
-    instructions
   end
+  [instructions_lookup, ingredient_lookup]
+end
 
 def search_recipe(ingredients)
+  recipes = {}
+  ingredient_map = {}
+
   # Extract the recipes for six types of alcohol
-  vodka_recipes = get_recipes('vodka')
-  whiskey_recipes = get_recipes('whiskey')
-  rum_recipes = get_recipes('rum')
-  tequila_recipes = get_recipes('tequila')
-  brandy_recipes = get_recipes('brandy')
-  gin_recipes = get_recipes('gin')
+  vodka_recipes = get_recipes('vodka', recipes, ingredient_map)
+  whiskey_recipes = get_recipes('whiskey', recipes, ingredient_map)
+  rum_recipes = get_recipes('rum', recipes, ingredient_map)
+  tequila_recipes = get_recipes('tequila', recipes, ingredient_map)
+  brandy_recipes = get_recipes('brandy', recipes, ingredient_map)
+  gin_recipes = get_recipes('gin', recipes, ingredient_map)
 
-  found = false
-  all_recipes = [vodka_recipes, whiskey_recipes, rum_recipes, tequila_recipes, brandy_recipes, gin_recipes]
-  all_recipes.each do |recipes|
-    recipes.each do |recipe|
-      # Check if all the ingredients are present in the recipe
-      all_ingredients_present = true
-      ingredients.each do |ingredient|
-        unless recipe.include?(ingredient)
-          all_ingredients_present = false
-          break
-        end
-      end
-
-      # If all the ingredients are present, print the recipe and set found to true
-      if all_ingredients_present
-        puts recipe
-        found = true
-      end
+  shared_recipes = []
+  ingredients.each do |ingredient|
+    if ingredient_map[ingredient].nil?
+      break
+    end
+    if shared_recipes.empty?
+      shared_recipes = ingredient_map[ingredient]
+    else
+      shared_recipes = shared_recipes & ingredient_map[ingredient]
     end
   end
-
-  # If no recipe was found, print a message indicating that no recipe could be found with the given ingredients
-  unless found
+  
+  if shared_recipes.empty?
     puts "No recipe found with the given ingredients."
+  else
+    shared_recipes.each do |recipe_id|
+      puts recipes[recipe_id]
+      puts
+    end
   end
 end
+
 
 puts "Enter a list of ingredients (separated by comma):"
 ingredients_string = gets.chomp
